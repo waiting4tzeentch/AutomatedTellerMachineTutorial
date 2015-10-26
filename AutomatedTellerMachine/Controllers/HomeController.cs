bcde627e9818aede0edb0e718259daae93242227
Michael Opper Moser <michael.opper.moser@gmail.com>
2015-10-25 21:46:54 -0400

    completed Understanding Controller Actions

--- AutomatedTellerMachine/Controllers/HomeController.cs
+++ AutomatedTellerMachine/Controllers/HomeController.cs
@@ -8,23 +8,31 @@ namespace AutomatedTellerMachine.Controllers
 {
     public class HomeController : Controller
     {
+        // GET /home/index
         public ActionResult Index()
         {
             return View();
         }
 
+        // GET /home/about
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
-
+         
+        // GET /home/contact
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Foo ()
+        {
+            return View("About");
         }
     }
 }
